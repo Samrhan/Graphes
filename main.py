284e970dc06de2eb7f9933e39037b289a5fc46af
Samrhan <samuel.bader@efrei.net>
2020-10-03 16:37:28 +0200

    Ajout de l'input utilisateur

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import os
 
+
+class Main:
+    def __init__(self):
+        self.graph_number = self.ask_user()
+        print(self.graph_number)
+
+    def ask_user(self):
+        n_graph = self.get_graph_number()
+        correct_answer = False
+        answer = -1
+        while not correct_answer:
+            try:
+                answer = int(input(f"Choisissez le numéro du graphe (1-{n_graph})"))
+                if answer <= 0 or answer > n_graph:
+                    raise ValueError
+                else:
+                    correct_answer = True
+            except ValueError:
+                print(f"Entrez un chiffre entre 1 et {n_graph} !")
+
+        return answer
+
+    @staticmethod
+    def get_graph_number():
+        return len(os.listdir('graphes'))
+
+
+Main()
